e3a2af9624403e608ef88f361f7c18b37cb39e3f
Joey Rudd <emailjoeyrudd@gmail.com>
2025-09-29 11:17:32 -0400

    Implement getMove in PlayerRandom class

--- src/ca/yorku/eecs3311/assignment1/othello/PlayerRandom.java
+++ src/ca/yorku/eecs3311/assignment1/othello/PlayerRandom.java
@@ -3,6 +3,7 @@ package ca.yorku.eecs3311.assignment1.othello;
 import java.util.ArrayList;
 import java.util.Random;
 
+
 /**
  * PlayerRandom makes a move by first determining all possible moves that this
  * player can make, putting them in an ArrayList, and then randomly choosing one
@@ -13,7 +14,25 @@ public class PlayerRandom {
 	
 	private Random rand = new Random();
 
-	public Move getMove() {
-		return null;
+	public Move getMove(OthelloBoard board, char player) {
+		ArrayList<Move> possibleMoves = new ArrayList<>();
+		
+		// search for possible moves
+		for(int row = 0; row < Othello.DIMENSION-1; row++) {
+			for(int col = 0; col < Othello.DIMENSION-1; col++) {
+				if (board.isValidMove(row, col, player)) {
+					possibleMoves.add(new Move(row, col));
+				}
+			}
+		}
+ 
+		// if no possible moves found return null
+		if (possibleMoves.isEmpty()) {
+			return null;
+		}
+		
+		// randomly select a move from possibleMoves list
+		int random = rand.nextInt(possibleMoves.size());
+		return possibleMoves.get(random);
 	}
 }
